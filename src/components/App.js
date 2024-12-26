6a4aabfef74a6bab79f3e027eb1c52dec81fb8d2
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-26 20:01:58 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ const App = () => {
   return (
     <PageLayout
       header={<h1>Welcome to my website</h1>}
-      footer={<p></p>}
+      footer={<p>© 2023 My Website. All rights reserved.</p>}
     >
-      <p>This is content of website.</p>
+      <p>This is content of my website.</p>
     </PageLayout>
   );
 };
