31e5ad9f9862be155f5c481d484c1902069aa652
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-26 19:59:49 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ const App = () => {
   return (
     <PageLayout
       header={<h1>Welcome to my website</h1>}
-      footer={<p>This is the content of my website.</p>}
+      footer={<p></p>}
     >
-      {/* <p>This is content of website.</p> */}
+      <p>This is content of website.</p>
     </PageLayout>
   );
 };
